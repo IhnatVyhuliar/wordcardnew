0d9941e46d73066b11b72c234aa07d2daacb3b71
VigularIgnat <ignatvigular@gmail.com>
2023-12-10 16:02:11 +0100

    Adding meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,15 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'WordCard') }}</title>
-
+        <meta name="robots" content="index, follow">
+        <meta name="keywords" content="wordcard, languages, flashcards, cards, studying, folders, flash, every, any, way to learn, learn, learning languages, english, polish, deutsch, german, ukrainian, chinese, korean, definition, translation">
+        <meta name='author' content="Ihnat Vyhuliar">
+        <meta name="description" content="Wordcard - your way to learn languages, flashcards and folders. Platform for studying every language">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
 
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />    
+        <link rel="shortcut icon" href="{{Storage::url('public/cards/wordcard.png')}}">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
