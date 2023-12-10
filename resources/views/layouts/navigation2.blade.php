89938901e1f14762d55c1201c49044a231d05ff4
VigularIgnat <ignatvigular@gmail.com>
2023-12-10 18:25:15 +0100

    adding favicon

--- resources/views/layouts/navigation2.blade.php
+++ resources/views/layouts/navigation2.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
-    <link id="favicon" rel="shortcut icon" href="{{Storage::url('public/cards/wordcard.png')}}">
-   
+    <link rel="shortcut icon" href="{{Storage::url('public/cards/wordcard.png')}}">
+    
     <title>WordCard</title>
 </head>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tailwindcss.com"></script>
+    <meta name="robots" content="index, follow">
+    <meta name="keywords" content="wordcard, languages, flashcards, words, card, word, card word, cards, studying, folders, flash, every, any, way to learn, learn, learning languages, english, polish, deutsch, german, ukrainian, chinese, korean, definition, translation">
+    <meta name='author' content="Ihnat Vyhuliar">
+    <meta name="description" content="Platform for studying every language">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />    
+    <link rel="shortcut icon" href="{{Storage::url('public/cards/wordcard.png')}}">
     <title>WordCard</title>
 </head>
 
