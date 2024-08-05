7bed7eab1835f1edcf4cdfeabf10a0fb8d00e828
VigularIgnat <ignatvigular@gmail.com>
2024-08-05 13:22:36 +0200

    Fixing bug with follow

--- app/Http/Controllers/Follow/FolderFollowerController.php
+++ app/Http/Controllers/Follow/FolderFollowerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Follow;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Folder_follower;
 
 use App\Models\Folder;
 use Illuminate\Support\Facades\Auth;
